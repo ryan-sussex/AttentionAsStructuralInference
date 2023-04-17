2b9e1669a4f4f9504ab723b3f571c1b28a081197
Ryan <rs773@sussex.ac.uk>
2023-04-17 14:19:03 +0100

    update to modifiable attention

--- main.py
+++ main.py
@@ -1,52 +1,59 @@
-from torch.nn import MultiheadAttention
 import torch
 from torch.nn import MSELoss
 import torch.optim as optim
 
 from data import Regression, SequenceRegression
+from models import CausalSelfAttention
+
 
 N_BATCH = 1
 N_SEQ = 5
 EMBED_DIM = 2
 
-attn = MultiheadAttention(embed_dim=EMBED_DIM, num_heads=1, batch_first=True)
+
+class AttentionConfig():
+    n_embd=EMBED_DIM
+    n_head=1
+    block_size=1
+
+
+attn = CausalSelfAttention(AttentionConfig())
 regr_problem = Regression(input_dim=EMBED_DIM, output_dim=EMBED_DIM)
 seq_regr_problem = SequenceRegression(
     input_dim=EMBED_DIM, output_dim=EMBED_DIM, sequence_length=N_SEQ)
 optimizer = optim.Adam(attn.parameters(), lr=1e-3)
 loss = MSELoss()
 
-if __name__ == "__main__":
-
+def linear_regression():
     # Example of simple linear regression
-    N_SAMPLES = 100
+    N_SAMPLES = 500
     for _ in range(N_SAMPLES):
         X, y = regr_problem.sample(batch_size=1000)
-
-        query = X
-        key = query.clone()
-        value = query.clone()
-        output, attn_weights = attn(query, key, value)
+        output = attn(X)
         out: torch.Tensor = loss(output, y)
         out.backward()
         optimizer.step()
         print(out.item())
 
-    # Example of regression where the input of the regression is chosen at random
-    N_SAMPLES = 1000
+def seq_regression():
+    N_SAMPLES = 500
     for _ in range(N_SAMPLES):
         X, y = seq_regr_problem.sample(batch_size=1000)
-
-        query = X
-        key = query.clone()
-        value = query.clone()
-        output, attn_weights = attn(query, key, value)
+        output = attn(X)
+        # output, attn_weights = attn(query, key, value)
         # Pick a single sequence element for y_prediction
         output_pred = output[:, 1, :]
         out: torch.Tensor = loss(output_pred, y)
         out.backward()
         optimizer.step()
         print(out.item())
+
+
+if __name__ == "__main__":
+
+    linear_regression()
+    seq_regression()
+    # Example of regression where the input of the regression is chosen at random
 
     print(seq_regr_problem.weight_matrix)
     for param in attn.parameters():
