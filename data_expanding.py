faec22784c93801b5dc176cd9ccec3e87d5cd9cc
Ryan <rs773@sussex.ac.uk>
2023-04-29 21:26:28 +0100

    move expanding stuff to new files

---
+++ data_expanding.py
@@ -0,0 +1,116 @@
+"""
+Helper classes for simulating regression problems.
+"""
+from typing import Optional
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from torch.distributions import Categorical
+
+import random
+
+
+STD = 0.01
+
+class Regression():
+
+    def __init__(
+            self,
+            input_dim: int,
+            output_dim: int,
+            weight_matrix: Optional[Tensor] = None
+        ):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        if weight_matrix is None:
+            self.weight_matrix = torch.rand(
+                (output_dim, 1)
+            ) * 10
+
+    def sample_X(self, batch_size: int) -> Tensor:
+        return torch\
+            .normal(mean=torch.ones(batch_size, self.input_dim), std=STD)
+
+    def regr_func(self, X):
+        return F.linear(self.weight_matrix, X)
+
+    def sample(self, batch_size: int = 1):
+        with torch.no_grad():
+            X = self.sample_X(batch_size)
+            y = self.regr_func(X).t()
+
+            # Put in empty sequence dim
+            X = X[:, None, :]
+            y = y[:, None]
+        return X, y
+
+
+class AutoRegression(Regression):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        sequence_length: int = 10,
+        weight_matrix: Optional[Tensor] = None,
+        ingroup_size: int = 1
+    ):
+        super().__init__(input_dim, output_dim, weight_matrix)
+        self.autoregressive_matrix = (
+            torch.randn(
+                (input_dim, input_dim)
+            ) * 10
+        )
+        self.sequence_length = sequence_length
+        self.ingroup_size = ingroup_size
+
+    def sample_X(self, batch_size: int) -> Tensor:
+        outgroup_size = self.sequence_length - self.ingroup_size
+
+        in_group = [ torch.normal(
+            mean=torch.ones(batch_size, 1, self.input_dim)* 10,
+            std=.1
+        )
+        ]
+        outgroup = torch.normal(
+            mean=torch.zeros(batch_size, outgroup_size, self.input_dim),
+            std=.1
+        )
+
+        for i in range(1, self.ingroup_size):
+            new = (
+                in_group[i - 1] @ self.autoregressive_matrix
+                + torch.normal(
+                    mean=torch.ones(batch_size, 1, self.input_dim),
+                    std=STD
+                )
+            )
+            in_group.append(new)
+
+        in_group = [outgroup] + in_group  
+        X = torch.cat(in_group, dim=1)
+        return X
+
+    def sample(self, batch_size: int = 1):
+        with torch.no_grad():
+            X = self.sample_X(batch_size)
+            x = X[:, self.sequence_length - self.ingroup_size, -1]
+            # print(self.sequence_length - self.ingroup_size)
+            for i in range(1, self.ingroup_size):
+                # print(self.sequence_length - self.ingroup_size + i)
+                # x = x + X[:, self.sequence_length - self.ingroup_size + i, -1]
+                pass
+            x = x[:, None]
+            y = self.regr_func(x).t()
+        # randomly select a single y
+        # shuffle X
+        # print(X.size())
+        idx = list(range(self.sequence_length - 1))
+        random.shuffle(idx)
+        # raise
+        idx = idx + [self.sequence_length - 1]
+        self.record = {"idx": idx}
+        # print(idx)
+        # raise
+        # X = X[:, idx, :]
+        return X, y
+
---
+++ run_expanding.py
@@ -0,0 +1,88 @@
+import torch
+torch.manual_seed(4)
+
+from torch.nn import MSELoss
+import torch.optim as optim
+
+from data import AutoRegression
+from models import CausalSelfAttention, ExpandingAttention, EfficientExpandingAttention
+
+
+
+N_BATCH = 1
+BATCH_SIZE = 1
+N_SAMPLES = 5000
+
+N_SEQ = 100
+N_AUTOREGRESS = 2
+EMBED_DIM = 300
+OUTPUT_DIM = 1
+LR = 1e-3
+
+RECORD_EVERY = 200
+
+
+class AttentionConfig():
+    n_embd=EMBED_DIM
+    n_head=1
+    block_size=1
+
+att_config = AttentionConfig()
+
+auto_regr_problem = AutoRegression(
+    input_dim=EMBED_DIM, 
+    output_dim=OUTPUT_DIM,
+    sequence_length=N_SEQ, 
+    ingroup_size=N_AUTOREGRESS
+)
+
+
+loss_fn = MSELoss(reduction="mean")
+
+def seq_regression(problem, attention_model):
+    optimizer = optim.Adam(attention_model.parameters(), lr=LR, weight_decay=2)
+
+    training_history = []
+    for batch_no in range(N_SAMPLES):
+        optimizer.zero_grad()
+
+        X, y = problem.sample(batch_size=BATCH_SIZE)
+        output = attn(X)
+        # Only use the last node for prediction
+        output_pred = output[:, - 1, :]
+        
+        loss: torch.Tensor = loss_fn(output_pred, y)
+        loss.backward()
+        optimizer.step()
+        if batch_no % RECORD_EVERY == 0:
+            # if  loss.item() > 0:
+            print(
+                f"model:{repr(attention_model)} batch_no:{batch_no} mse:{loss}"
+            )
+            training_history.append(loss.item())
+        #     print(X)
+        #     print(y)
+            print("window", attention_model.record["window"].item())
+            print("iters", attention_model.record["iters"])
+            # print("k", attention_model.record["k"])
+            print(attention_model.record["attention"])
+
+        # # print(problem.record)
+        #     print(attention_model.alpha)
+        #     print(attention_model.beta)
+
+    return training_history
+
+
+if __name__ == "__main__":
+    training_dct = {}
+    attn = EfficientExpandingAttention(AttentionConfig())
+    training_dct["expanding"] = seq_regression(auto_regr_problem, attention_model=attn)
+    attn = CausalSelfAttention(AttentionConfig())
+    training_dct["standard"] = seq_regression(auto_regr_problem, attention_model=attn)
+    attn = ExpandingAttention(AttentionConfig())
+    training_dct["expanding"] = seq_regression(auto_regr_problem, attention_model=attn)
+
+    import json
+    with open("expanding.json", mode="w") as f:
+        json.dump(training_dct, f)
