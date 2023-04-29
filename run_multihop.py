640369b2cf43ca8fc6c905fdd39e81c366caf291
Ryan <rs773@sussex.ac.uk>
2023-04-29 21:29:43 +0100

    multihop files

--- run_multihop.py
+++ run_multihop.py
@@ -5,21 +5,21 @@ from torch.nn import MSELoss
 import torch.optim as optim
 
 from data import AutoRegression
-from models import CausalSelfAttention, LongAttention, ExpandingAttention, EfficientExpandingAttention
+from models import CausalSelfAttention, LongAttention
 
 
 
 N_BATCH = 1
-BATCH_SIZE = 1
-N_SAMPLES = 5000
+BATCH_SIZE = 200
+N_SAMPLES = 50000
 
-N_SEQ = 100
-N_AUTOREGRESS = 2
-EMBED_DIM = 300
+N_SEQ = 8
+N_AUTOREGRESS = 3
+EMBED_DIM = 1
 OUTPUT_DIM = 1
 LR = 1e-3
 
-RECORD_EVERY = 200
+RECORD_EVERY = 1000
 
 
 class AttentionConfig():
@@ -55,33 +55,18 @@ def seq_regression(problem, attention_model):
         loss.backward()
         optimizer.step()
         if batch_no % RECORD_EVERY == 0:
-            # if  loss.item() > 0:
             print(
                 f"model:{repr(attention_model)} batch_no:{batch_no} mse:{loss}"
             )
             training_history.append(loss.item())
-        #     print(X)
-        #     print(y)
-            print("window", attention_model.record["window"].item())
-            print("iters", attention_model.record["iters"])
-            # print("k", attention_model.record["k"])
-            print(attention_model.record["attention"])
-
-        # # print(problem.record)
-        #     print(attention_model.alpha)
-        #     print(attention_model.beta)
 
     return training_history
 
 
 if __name__ == "__main__":
     training_dct = {}
-    attn = EfficientExpandingAttention(AttentionConfig())
-    training_dct["expanding"] = seq_regression(auto_regr_problem, attention_model=attn)
-    # attn = CausalSelfAttention(AttentionConfig())
-    # training_dct["standard"] = seq_regression(auto_regr_problem, attention_model=attn)
-    attn = ExpandingAttention(AttentionConfig())
-    training_dct["expanding"] = seq_regression(auto_regr_problem, attention_model=attn)
+    attn = CausalSelfAttention(AttentionConfig())
+    training_dct["standard"] = seq_regression(auto_regr_problem, attention_model=attn)
     attn = LongAttention(AttentionConfig())
     training_dct["long"] = seq_regression(auto_regr_problem, attention_model=attn)
 
